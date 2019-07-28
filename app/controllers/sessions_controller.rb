7642bfff00ffdfb373e2a0d9fceedf1fb14bdfb8
Casey Hannan <Hannan.Casey@gmail.com>
2019-07-28 14:20:45 -0400

    implementation advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+      user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      # remember user
       redirect_to user
       # Log the user in and redirect to the user's show page.
     else
@@ -17,7 +19,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
