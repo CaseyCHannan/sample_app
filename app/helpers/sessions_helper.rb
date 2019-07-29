a6c38a42d7e40fe9d0ca77cbcf9b0b946114e02b
Casey Hannan <Hannan.Casey@gmail.com>
2019-07-29 13:33:42 -0400

    Finished user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,6 +33,22 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Logs out the current user.
   def log_out
     forget(current_user)
